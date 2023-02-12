b55b5d57166d5d064ae5d4fefe970419880d8627
Charles Cao <caotaotao@gmail.com>
2023-02-12 13:55:25 -0500

    update tests

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,13 +1,14 @@
 from lagrange.lagrange_client import LagrangeClient
+import pytest
 
 
 def test_get_dataset():
     lagrange_client = LagrangeClient()
     dataset = lagrange_client.get_dataset("charles-hello")
-    assert (dataset.name, "charles-hello")
+    assert dataset.name == "charles-hello"
 
 
 def test_dataset_not_found():
     lagrange_client = LagrangeClient()
     dataset = lagrange_client.get_dataset("charles-hell")
-    assert (dataset, None)
+    assert dataset is None
